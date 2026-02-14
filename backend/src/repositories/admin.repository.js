ac97ba5bf81057b5b9a5fd80a8b873188197dc1e
Marco <bitar.marco21@gmail.com>
2026-02-14 16:02:37 +0200

    fixed the create to use the user repo function

--- backend/src/repositories/admin.repository.js
+++ backend/src/repositories/admin.repository.js
@@ -3,18 +3,15 @@
 import { pool } from '../config/db.js';
 import { Admin } from '../entities/Admin.js';
 import { User } from '../entities/User.js';
+import { UserRepository } from '../repositories/user.repository.js';
 
 export class AdminRepository {
     async createAdmin(data) {
         const { admin_fname, admin_lname, user } = data;
 
         // Step 1: Create the user first
-        const userSql = `INSERT INTO users (user_email, user_password, user_phone,
-            user_role, isactive, created_at) VALUES ($1, $2, $3, $4, $5, NOW()) 
-            RETURNING user_id, user_email, user_phone, user_role, isactive`;
-        const userValues = [user.user_email, user.user_password, user.user_phone, 'admin', true];
-        const { rows: userRows } = await pool.query(userSql, userValues);
-        const createdUser = new User(userRows[0]);
+        const userRepository = new UserRepository();
+        const createdUser = await userRepository.createUser(user);
 
         // Step 2: Create the admin with the user_id
         const adminSql = `INSERT INTO admins (admin_id, admin_fname, admin_lname)
